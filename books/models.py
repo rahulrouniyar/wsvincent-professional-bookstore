8d88778105532f54213229ef1c50af7aab7e7058
rahulrouniyar <075bei027.rahul@pcampus.edu.np>
2025-01-10 14:11:14 +0545

    Bookstore project following django for professionals

---
+++ books/models.py
@@ -0,0 +1,33 @@
+import uuid
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+
+# Create your models here.
+class Book(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover = models.ImageField(upload_to="covers/", blank=True)
+
+    class Meta:
+        permissions = [
+            ("special_status", "Can read all books"),
+        ]
+
+    def __str__(self) -> str:
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("books_detail", args=[str(self.id)])
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="reviews")
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    review = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.review
